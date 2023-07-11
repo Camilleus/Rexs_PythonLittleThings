6fc85bb9703899519e2c3c57daf700ac5d0ef042
Camilleus <camilleusrex@gmail.com>
2023-07-11 19:31:02 +0200

    added BMICalculatorFR

---
+++ BMICalculatorFR.py
@@ -0,0 +1,20 @@
+print("Salut Monsieur/Madame, Avez vous envie de connaître votre IMC(BMI) ? Introduisez vos données ci-dessous:")
+
+weight = float(input("Quel est ton poids? (kg) : "))
+height = float(input("Quelle est ta hauteur? (cm) : "))
+bmi = float(weight / ((height * 0.01) * (height * 0.01)))
+
+while bmi > 0:
+    if bmi < 18.5:
+        print("Vous etes Trop maigre!!!!")
+    elif bmi < 25:
+        print("Vous devez etre totalement satisfait abec votre poids!!!!")
+    elif bmi < 30:
+        print("Propablement vous avez quelques kilos en trop.")
+    elif bmi < 40:
+        print("Vous etes obèse!!!!")
+    else:
+        print("Vous avez obésité morbide!! C'est dangereux!!")
+    break
+
+# J'aime le programming et Le Python
