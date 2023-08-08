99213fae6a3c363db47238f8e6a0cdf764a942a8
Camilleus <camilleusrex@gmail.com>
2023-08-08 19:37:34 +0200

    created find_all_emails function

--- TryingNewThingsFile.py
+++ TryingNewThingsFile.py
@@ -1,14 +1,8 @@
 import re
 
 
-def replace_spam_words(text, spam_words):
-    for word in spam_words:
-        escaped_word = word.replace('+', r'\+')
-        word_pattern = re.compile(rf'\b{escaped_word}\b', re.IGNORECASE)
-        text = word_pattern.sub('*' * len(word), text)
-    return text
-
-text = "Python is an amazing programming language, but beware of spam words like JAVA, c++, and javascript."
-spam_words = ["java", "c++", "javascript"]
-result = replace_spam_words(text, spam_words)
-print(result)
+def find_all_emails(text):
+    email_pattern = re.compile(
+        r'\b[A-Za-z0-9._%+-]+@[A-Za-z]+\.[A-Za-z]{2,}\b')
+    result = email_pattern.findall(text)
+    return result
