df94e4414b5e7832aa0b06113c479059461c5f3b
Camilleus <camilleusrex@gmail.com>
2023-08-08 19:37:56 +0200

    added find_all_emails function

--- LesAutres.py
+++ LesAutres.py
@@ -221,3 +221,11 @@ def replace_spam_words(text, spam_words):
     return text
 
 """
+"""
+import re
+
+def find_all_emails(text):
+    all_emails = re.compile(r"[a-zA-Z][a-zA-Z0-9_.]+@[a-z]+\.[a-z]{2,}")
+    result = all_emails.findall(text)
+    return result
+"""
