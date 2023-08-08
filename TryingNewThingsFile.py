54ed915fabbd2bd26adbcb4297927fcba64b05e0
Camilleus <camilleusrex@gmail.com>
2023-08-08 20:05:35 +0200

    created find_all_phones function

--- TryingNewThingsFile.py
+++ TryingNewThingsFile.py
@@ -1,7 +1,12 @@
 import re
 
 
-def find_all_emails(text):
-    all_emails = re.compile(r"[a-zA-Z][a-zA-Z0-9_.]+@[a-z]+\.[a-z]{2,}")
-    result = all_emails.findall(text)
+def find_all_phones(text):
+    phone_pattern = re.compile(r'\+380\(\d{2}\)777-\d{1,2}-\d{2,3}')
+    result = phone_pattern.findall(text)
     return result
+
+
+text = "Irma +380(67)777-7-771 second +380(67)777-77-77 aloha a@test.com abc111@test.com.net +380(67)111-777-777+380(67)777-77-787"
+phones = find_all_phones(text)
+print(phones)
