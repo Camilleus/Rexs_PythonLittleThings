ded399cadf99eced03a57ae314e60f9dd24ee185
Camilleus <camilleusrex@gmail.com>
2023-08-08 21:41:06 +0200

    added find_all_phones function

--- LesAutres.py
+++ LesAutres.py
@@ -229,3 +229,18 @@ def find_all_emails(text):
     result = all_emails.findall(text)
     return result
 """
+"""
+import re
+
+
+def find_all_phones(text):
+    all_phones = re.compile(r'\+380\(\d{2}\)777-\d{1,2}-\d{2,3}')
+    result = all_phones.findall(text)
+    return result
+
+
+text = "Irma +380(67)777-7-771 second +380(67)777-77-77 aloha a@test.com abc111@test.com.net +380(67)111-777-777+380(67)777-77-787"
+phones = find_all_phones(text)
+print(phones)
+
+"""
