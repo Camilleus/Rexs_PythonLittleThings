f282b2d8761d7127cb09e596df3a3e4aae8ed655
Camilleus <camilleusrex@gmail.com>
2023-08-08 19:03:14 +0200

    created replace_spam_words function

--- TryingNewThingsFile.py
+++ TryingNewThingsFile.py
@@ -1,30 +1,14 @@
 import re
 
 
-def find_word(text, word):
-    search_result = re.search(rf'\b{re.escape(word)}\b', text, re.IGNORECASE)
+def replace_spam_words(text, spam_words):
+    for word in spam_words:
+        escaped_word = word.replace('+', r'\+')
+        word_pattern = re.compile(rf'\b{escaped_word}\b', re.IGNORECASE)
+        text = word_pattern.sub('*' * len(word), text)
+    return text
 
-    if search_result:
-        result_dict = {
-            'result': True,
-            'first_index': search_result.start(),
-            'last_index': search_result.end() - 1,
-            'search_string': search_result.group(),
-            'string': text
-        }
-    else:
-        result_dict = {
-            'result': False,
-            'first_index': None,
-            'last_index': None,
-            'search_string': word,
-            'string': text
-        }
-
-    return result_dict
-
-
-# Test the find_word function
-text = "Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0."
-word = "Python"
-print(find_word(text, word))
+text = "Python is an amazing programming language, but beware of spam words like JAVA, c++, and javascript."
+spam_words = ["java", "c++", "javascript"]
+result = replace_spam_words(text, spam_words)
+print(result)
