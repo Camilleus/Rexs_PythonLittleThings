86c3e320afdee7436e4129000fec9b006c89b266
Camilleus <camilleusrex@gmail.com>
2023-08-09 19:53:00 +0200

    added write_employees_to_file function

--- LesAutres.py
+++ LesAutres.py
@@ -244,3 +244,16 @@ phones = find_all_phones(text)
 print(phones)
 
 """
+"""
+def write_employees_to_file(employee_list, path):
+    with open(path, "w") as file:
+        for department in employee_list:
+            for employee in department:
+                file.write(employee + "\n")
+    file.close()
+
+
+employee_list = [['Robert Stivenson,28',
+                  'Alex Denver,30'], ['Drake Mikelsson,19']]
+write_employees_to_file(employee_list, "employee_data.txt")
+"""
