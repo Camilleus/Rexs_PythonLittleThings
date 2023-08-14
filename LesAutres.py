1dcfc9a73c4b124bec2cd78366a496e8689bfc1a
Camilleus <camilleusrex@gmail.com>
2023-08-14 22:30:01 +0200

    repaired find_all_phones function?

--- LesAutres.py
+++ LesAutres.py
@@ -230,9 +230,10 @@ import re
 
 
 def find_all_phones(text):
-    all_phones = re.compile(r'\+380\(\d{2}\)777-\d{1,2}-\d{2,3}')
-    result = all_phones.findall(text)
+    result re.findall (r"\+380\([0-9]{2}\)[0-9]{3}\-[0-9]{1}\-[0-9]{3}|\+380\([0-9]
+{2}\)[0-9]{3}\-[0-9]{2}\-[0-9]{2}", text)
     return result
+
 
 
 text = "Irma +380(67)777-7-771 second +380(67)777-77-77 aloha a@test.com abc111@test.com.net +380(67)111-777-777+380(67)777-77-787"
