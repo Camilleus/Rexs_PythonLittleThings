f5058a4a5d65c8f1f7f2d8c98c704a8a4c158a3d
Camilleus <camilleusrex@gmail.com>
2023-08-22 18:33:07 +0200

    added data_preparation function

--- LesAutres.py
+++ LesAutres.py
@@ -529,3 +529,18 @@ def solve_riddle(riddle, word_length, start_letter, reverse=False):
     
     return ""
 """
+"""
+def data_preparation(data):
+    result = []
+
+    for sublist in data:
+        if len(sublist) > 2:
+            sublist.remove(min(sublist))
+            sublist.remove(max(sublist))
+            result.extend(sublist)
+        else:
+            sublist.extend(sublist)
+
+    result.sort(reverse=True)
+    return result
+"""
