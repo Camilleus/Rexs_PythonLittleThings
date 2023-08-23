04c8e58edd51a259b077ef9879d59d09eec6a31f
Camilleus <camilleusrex@gmail.com>
2023-08-23 20:05:18 +0200

    created time_since function and added print values

--- DaysFromBirthCounter.py
+++ DaysFromBirthCounter.py
@@ -1,4 +1,12 @@
 from datetime import datetime, timedelta
-particular_date = datetime(2002, 8, 20)
-new_date = datetime.today() - particular_date
-print (new_date.days)
+
+BD = input("What si your Brithdate? (yyyy/mm/dd)")
+def time_since(BD):
+    date_obj = datetime.strptime(BD, '%Y/%m/%d')
+    delta = datetime.now() - date_obj
+    return delta
+
+time_elapsed = time_since(BD)
+print(f"From {BD} passed: {time_elapsed.days} days.")
+print(f"From {BD} passed: {time_elapsed.days/30} months.")
+print(f"From {BD} passed: {time_elapsed.days/365} years.")
