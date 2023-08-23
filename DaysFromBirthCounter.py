cafec73ca8e53edd1ebfd59a7509783f0be00980
Camilleus <camilleusrex@gmail.com>
2023-08-23 21:00:09 +0200

    written new function but still need to fix it in order to work properly

--- DaysFromBirthCounter.py
+++ DaysFromBirthCounter.py
@@ -1,8 +1,11 @@
 from datetime import datetime, timedelta
 
 BD = input("What si your Brithdate? (yyyy/mm/dd)")
+date_obj = datetime.strptime(BD, '%Y/%m/%d')
+
+"""
+#ITS FIRST VERSION OF IT    
 def time_since(BD):
-    date_obj = datetime.strptime(BD, '%Y/%m/%d')
     delta = datetime.now() - date_obj
     return delta
 
@@ -10,3 +13,25 @@ time_elapsed = time_since(BD)
 print(f"From {BD} passed: {time_elapsed.days} days.")
 print(f"From {BD} passed: {time_elapsed.days/30} months.")
 print(f"From {BD} passed: {time_elapsed.days/365} years.")
+"""
+def time_since(date_obj):
+    now = datetime.now()
+    delta = now - date_obj
+
+    years = (now.year - date_obj.year)
+    if now.month < date_obj.month or (now.month == date_obj.month and now.day < date_obj.day):
+        years -= 1
+
+    months = years * 12 + now.month - date_obj.month
+    days = delta.days - years * 365
+
+    hours, remainder = divmod(delta.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    return years, months, days, hours, minutes, seconds
+
+years, months, days, hours, minutes, seconds = time_since(date_obj)
+print(
+    f"From {date_obj} passed: {years} years, {months} months, {days} days, {hours} hours, {minutes} minutes and {seconds} seconds.")
+
+
