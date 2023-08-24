53912c102cc28111ac4806c4ef4631b33d8c8242
Camilleus <camilleusrex@gmail.com>
2023-08-24 21:30:43 +0200

    added get_employees_by_profession function

--- LesAutres.py
+++ LesAutres.py
@@ -605,3 +605,18 @@ def file_operations(path, additional_info, start_pos, count_chars):
         
     return read_data
 """
+"""
+import re
+
+
+def get_employees_by_profession (path, profession): 
+    with open(path, 'r') as file:
+        verses = file.readlines()
+        matchings = []
+        for verse in verses:
+            if verse.find(profession) > 0:
+                vers = verse.replace(profession, "") 
+                matchings.append(re.sub (r'\s+', '', vers))
+        return ' '.join(matchings)
+        
+"""
