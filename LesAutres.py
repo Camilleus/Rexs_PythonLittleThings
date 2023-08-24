a30c4fd929dd4a3d845a8cf66028bdd5237dc140
Camilleus <camilleusrex@gmail.com>
2023-08-24 19:50:05 +0200

    added sequence_buttons function

--- LesAutres.py
+++ LesAutres.py
@@ -571,3 +571,27 @@ def make_request(keys, values):
 
     return request_dict
 """
+"""
+def sequence_buttons(string):
+    char_buttons = {
+        '.': '1', ',': '11', '?': '111','!': '1111',':': '1111',
+        'A': '2', 'B': '22', 'C': '222',
+        'D': '3', 'E': '33', 'F': '333',
+        'G': '4', 'H': '44', 'I': '444',
+        'J': '5', 'K': '55', 'L': '555',
+        'M': '6', 'N': '66', 'O': '666',
+        'P': '7', 'Q': '77', 'R': '777', 'S': '7777',
+        'T': '8', 'U': '88', 'V': '888',
+        'W': '9', 'X': '99', 'Y': '999', 'Z': '9999',
+        ' ': '0'
+    }
+    
+    string = string.upper()
+    flush = []
+    
+    for char in string:
+        if char in char_buttons:
+            flush.append(char_buttons[char])
+    
+    return ''.join(flush) 
+"""
