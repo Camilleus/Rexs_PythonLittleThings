49198f2f0a193676c7cdc7dc3e8364c38211114f
Camilleus <camilleusrex@gmail.com>
2023-08-24 20:24:01 +0200

    added file_operations function

--- LesAutres.py
+++ LesAutres.py
@@ -595,3 +595,13 @@ def sequence_buttons(string):
     
     return ''.join(flush) 
 """
+"""
+def file_operations(path, additional_info, start_pos, count_chars):
+    with open(path, 'a') as file:
+        file.write(additional_info)
+    with open(path, 'r') as file:
+        file.seek(start_pos)  
+        read_data = file.read(count_chars) 
+        
+    return read_data
+"""
