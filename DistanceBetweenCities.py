6f7c7df602702123da0ea627a8ccdba8e9b61ece
Camilleus <camilleusrex@gmail.com>
2023-08-27 19:44:31 +0200

    changed fixed coordiantes to users coordinates

--- DistanceBetweenCities.py
+++ DistanceBetweenCities.py
@@ -7,16 +7,18 @@ arccos = math.acos
 sin = math.sin
 cos = math.cos
 
-lat1 = 50.45
-lon1 = 30.523
-lat2 = 51.5072
-lon2 = -0.1275
+lat1, lon1 = map(float, input(
+    "What are coordinates of your first city? (latitude and longtitude)").split())
+lat2, lon2 = map(float, input(
+    "What are coordinates of your second city? (latitude and longtitude)").split())
+
 
 lon1 = radians(lon1)
 lon2 = radians(lon2)
 lat1 = radians(lat1)
 lat2 = radians(lat2)
 
-distance = 6371.01 * arccos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2)  * cos(lon1 - lon2))
+distance = 6371.01 * arccos(sin(lat1) * sin(lat2) +
+                            cos(lat1) * cos(lat2) * cos(lon1 - lon2))
 
-print(distance)
+print(distance, " kilometers")
