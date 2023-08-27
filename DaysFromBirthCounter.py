49e641301362ec241668ab92037e744ccaf3f09e
Camilleus <camilleusrex@gmail.com>
2023-08-27 20:44:23 +0200

    #Doesn't work with future dates repair it son!

--- DaysFromBirthCounter.py
+++ DaysFromBirthCounter.py
@@ -1,19 +1,30 @@
 import datetime
+import math
 
-BD = input("What si your Brithdate? (yyyy/mm/dd)")
-date_obj = datetime.strptime(BD, '%Y/%m/%d')
+
+def count_leap_years(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return 1
+    else:
+        return 0
+
+
+# Doesn't work with future dates repair it son!
+BD = input("What is your Birthdate? (yyyy/mm/dd)")
+date_obj = datetime.datetime.strptime(BD, '%Y/%m/%d')
 
 
 def time_since(date_obj):
-    now = datetime.now()
+    now = datetime.datetime.now()
     delta = now - date_obj
 
-    years = (now.year - date_obj.year)
-    if now.month < date_obj.month or (now.month == date_obj.month and now.day < date_obj.day):
-        years -= 1
+    years = (math.floor(now.year - date_obj.year))
+    leap_years = sum(count_leap_years(year)
+                     for year in range(date_obj.year, now.year))
+    total_days = delta.days + leap_years
 
     months = now.month - date_obj.month
-    days = delta.days - years * 365
+    days = delta.days - years * 365 - leap_years
 
     hours, remainder = divmod(delta.seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
@@ -22,9 +33,8 @@ def time_since(date_obj):
 
 
 years, months, days, hours, minutes, seconds = time_since(date_obj)
-date_obj = date_obj.strftime("%d %B, %Y, %H:%M:%S")
-print(
-    f"From {date_obj} passed: {years} years, {months} months, {days} days, {hours} hours, {minutes} minutes and {seconds} seconds.")
+formatted_date_obj = date_obj.strftime("%d %B, %Y, %H:%M:%S")
+print(f"From {formatted_date_obj} passed: {years} years, {months} months, {days} days, {hours} hours, {minutes} minutes and {seconds} seconds.")
 
 
 """
