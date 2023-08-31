c3a0676bb3ca7375bd74281bd9c18ce1046f1ed9
Camilleus <camilleusrex@gmail.com>
2023-08-31 19:21:29 +0200

    months cannot be minus from now

--- DaysFromBirthCounter.py
+++ DaysFromBirthCounter.py
@@ -25,7 +25,11 @@ def time_since(date_obj):
 
     months = now.month - date_obj.month
     days = delta.days - years * 365 - leap_years
-
+    if months < 0:
+        months += 12
+        years -= 1
+    if days < 0 or days > 31:
+        days = days = delta.days - years * 365 - leap_years
     hours, remainder = divmod(delta.seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
 
