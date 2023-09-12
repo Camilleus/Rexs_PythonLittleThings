dad3258dd6e8e41edb91a58d3fa39774afabddea
CamilleusRex <camilleusrex@gmail.com>
2023-09-12 19:17:36 +0200

    created fib function woth iteration

--- TryingNewThingsFile.py
+++ TryingNewThingsFile.py
@@ -1,19 +1,31 @@
 import time
-from functools import lru_cache
 
 
-@lru_cache()
 def fibonacci(n):
     if n <= 1:
         return n
-    else:
-        return (fibonacci(n-1) + fibonacci(n-2))
+
+    fib_sequence = [0, 1]
+    for i in range(2, n + 1):
+        next_fib = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_fib)
+
+    return fib_sequence[n]
 
 
-start = time.time()
-n = int(input("Which number of Fibonacci sequence do you want to see? (If it is bigger than 20, this can take a little bit more time)"))
-for i in range(n+1):
-    print(f"{i}. Fibonacci sequence number is {fibonacci(i)}")
+while True:
+    try:
+        n = int(input("Which number of the Fibonacci sequence do you want to see? (If it is bigger than 20, this can take a little bit more time): "))
+        if n < 0:
+            print("Please enter a non-negative integer.")
+            continue
 
-end = time.time()
-print(f"This operation took {end - start} seconds")
+        start = time.time()
+        result = fibonacci(n)
+        end = time.time()
+
+        print(f"{n}. Fibonacci sequence number is {result}")
+        print(f"This operation took {end - start:.6f} seconds")
+
+    except ValueError:
+        print("Please enter a valid integer.")
