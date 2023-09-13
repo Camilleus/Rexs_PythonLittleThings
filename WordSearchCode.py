a194cc87150d5938713584f243efb1680bdbd30e
CamilleusRex <camilleusrex@gmail.com>
2023-09-13 19:12:40 +0200

    added counter of words

--- WordSearchCode.py
+++ WordSearchCode.py
@@ -1,10 +1,17 @@
-text = str(input())
-word = str(input())
+text = str(input("Enter the text: "))
+word = str(input("Enter the word to search for: "))
+counter = 0
 
-def search():
- if word in text:
-  return('Word found')
- else:
-  return('Word not found')
+
+def search(text, word):
+    global counter
+    if word in text:
+        for _ in text.split():
+            if _ == word:
+                counter += 1
+        return f'Word found {counter} times'
+    else:
+        return 'Word not found'
+
 
 print(search(text, word))
