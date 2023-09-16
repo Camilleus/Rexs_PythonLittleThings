6c1f8a22ba51e2c00cf22b9e6f007a261b928313
CamilleusRex <camilleusrex@gmail.com>
2023-09-16 12:58:55 +0200

    does it work?

--- backgroundremover.py
+++ backgroundremover.py
@@ -1,6 +1,5 @@
 from rembg import remove
 from PIL import Image
-image_input = Image.open(
-    r"image_path")
+image_input = Image.open(r"image_path")
 output = remove(image_input)
 output.save(r"image_output")
