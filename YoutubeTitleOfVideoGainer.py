ecd9cfe0bed5aab199c57756fbf9fe3d4a95bd67
CamilleusRex <camilleusrex@gmail.com>
2023-09-19 19:36:50 +0200

    added spotify function

--- YoutubeTitleOfVideoGainer.py
+++ YoutubeTitleOfVideoGainer.py
@@ -43,3 +43,43 @@ songs = get_songs_from_youtube_playlist(playlist_id, api_key)
 
 for song in songs:
     print(f"Title: {song['title']}, Artist: {song['artist']}")
+
+
+""" THE SAME BUT FOR SPOTIFY
+
+
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+def get_songs_from_spotify_playlist(username, playlist_id):
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="playlist-read-private"))
+
+    songs = []
+
+    try:
+        playlist = sp.user_playlist_tracks(username, playlist_id)
+        
+        for item in playlist['items']:
+            track = item['track']
+            song = {
+                "title": track['name'],
+                "artist": track['artists'][0]['name']
+            }
+            songs.append(song)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    return songs
+
+# Example usage of the function:
+username = "YOUR_SPOTIFY_USERNAME"
+playlist_id = "YOUR_SPOTIFY_PLAYLIST_ID"
+
+songs = get_songs_from_spotify_playlist(username, playlist_id)
+
+for song in songs:
+    print(f"Title: {song['title']}, Artist: {song['artist']}")
+
+"""
