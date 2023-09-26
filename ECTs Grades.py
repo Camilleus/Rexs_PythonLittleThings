f6baf06c2fc7c401ab67d9f2f0f1de9628a16083
CamilleusRex <camilleusrex@gmail.com>
2023-09-26 19:48:58 +0200

    created another version

--- ECTs Grades.py
+++ ECTs Grades.py
@@ -51,3 +51,35 @@ if ects_grade not in grades_points:
 else:
     print(f"Your Grade is {grade for key in grades_points}")
     print(f"Grade Description: {description for key in grades_points}")
+
+
+""" Another version of it
+
+
+
+def get_grade(key):
+    grade = {"A": 5, "B": 5, "C": 4, "D": 3, "E": 3, "FX": 2, "F": 1}
+    return grade.get(key, None)
+
+
+def get_description(key):
+    description = {
+        "A": "Perfectly",
+        "B": "Very good",
+        "C": "Good",
+        "D": "Satisfactorily",
+        "E": "Enough",
+        "FX": "Unsatisfactorily",
+        "F": "Unsatisfactorily",
+    }
+    return description.get(key, None)
+
+
+def get_student_grade(option):
+    if option == "grade":
+        return get_grade
+    elif option == "description":
+        return get_description
+    else:
+        return None
+"""
