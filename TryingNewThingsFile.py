84366bb011b3d8a7fcf3145c80fd56f416fc5410
CamilleusRex <camilleusrex@gmail.com>
2023-10-08 11:37:21 +0200

    #Userstring

--- TryingNewThingsFile.py
+++ TryingNewThingsFile.py
@@ -1,3 +1,4 @@
+from collections import UserString
 from collections import UserDict
 
 
@@ -8,3 +9,12 @@ class LookUpKeyDict(UserDict):
             if data[key] == value:
                 keys.append(key)
         return keys
+
+
+class NumberString(UserString):
+    def number_count(self):
+        count = 0
+        for char in self.data:
+            if char.isdigit():
+                count += 1
+        return count
