b98010b12223d92e8d7b5ca1e1efbb2fba06171c
CamilleusRex <camilleusrex@gmail.com>
2023-10-08 11:39:01 +0200

    added Userdict Userstring etc codes

--- LesAutres.py
+++ LesAutres.py
@@ -1056,5 +1056,42 @@ cat = Cat("Simon", 10)
 first_animal = Animal("Alex", 10)
 second_animal = Animal("Svenya", 10)
 first_animal.change_color("red")
+
 ###############################
+
+from collections import UserString
+from collections import UserDict
+
+
+class LookUpKeyDict(UserDict):
+    def lookup_key(data, value):
+        keys = []
+        for key in data:
+            if data[key] == value:
+                keys.append(key)
+        return keys
+
+
+class NumberString(UserString):
+    def number_count(self):
+        count = 0
+        for char in self.data:
+            if char.isdigit():
+                count += 1
+        return count
+
+
+class IDException(Exception):
+    pass
+
+
+def add_id(id_list, employee_id):
+    if employee_id.startswith("01"):
+        id_list.append(employee_id)
+        return id_list
+    else:
+        raise IDException
+
+######################################
+
 """
