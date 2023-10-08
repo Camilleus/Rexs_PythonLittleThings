b0b02f8dac9bf30d930635c3975f8ba3026bde2a
CamilleusRex <camilleusrex@gmail.com>
2023-10-08 11:36:56 +0200

    added payment function with lambda

--- LesAutres.py
+++ LesAutres.py
@@ -975,6 +975,13 @@ def amount_payment(payment):
     return total_payment
 
 
+from collections import UserList
+
+
+class AmountPaymentList(UserList):
+
+    def amount_payment(self):
+        return sum(filter(lambda x: x > 0, self.data))
 
 
 ################################
