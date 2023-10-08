3b5c9394e85d9afd27dbb809f5057b9a62934a2e
CamilleusRex <camilleusrex@gmail.com>
2023-10-08 11:32:54 +0200

    added another setter to the class

--- TryingNewThingsFile.py
+++ TryingNewThingsFile.py
@@ -1,4 +1,6 @@
 class Animal:
+    color = "white"
+
     def __init__(self, nickname, weight):
         self.nickname = nickname
         self.weight = weight
@@ -6,9 +8,13 @@ class Animal:
     def say(self):
         pass
 
-    def change_weight(self, new_weight):
-        self.weight = new_weight
+    def change_weight(self, weight):
+        self.weight = weight
+
+    def change_color(self, new_color):
+        Animal.color = new_color
 
 
-animal = Animal("Simon", 10)
-animal.change_weight(12)
+first_animal = Animal("Alex", 10)
+second_animal = Animal("Svenya", 10)
+first_animal.change_color("red")
