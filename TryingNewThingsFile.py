62e4ddf7f0b4bc244d3c3cbd1c871967e2fe0b66
CamilleusRex <camilleusrex@gmail.com>
2023-10-10 19:20:44 +0200

    added setter and getter for Point Class

--- TryingNewThingsFile.py
+++ TryingNewThingsFile.py
@@ -2,3 +2,23 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @x.setter
+    def x(self, new_x):
+        self.__x = new_x
+
+    @y.setter
+    def y(self, new_y):
+        self.__y = new_y
+
+
+p = Point(5, 5)
+print(p.x)
