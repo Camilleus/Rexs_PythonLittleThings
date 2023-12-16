1e5680b665f7dd5339e0a3e3e14b0d9ed86bf256
CamilleusRex <camilleusrex@gmail.com>
2023-12-16 14:18:51 +0100

    #async for

--- aiofile.py
+++ aiofile.py
@@ -23,3 +23,15 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
+
+
+# Podejście async for
+
+
+async def main():
+    async with async_open("hello.txt", 'r') as afp:
+        async for line in afp:
+            print(line)
+
+if __name__ == '__main__':
+    asyncio.run(main())
