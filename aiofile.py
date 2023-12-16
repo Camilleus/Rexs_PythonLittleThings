7c6d7f374f93e98d5218a8e8d1e85c09d49914fb
CamilleusRex <camilleusrex@gmail.com>
2023-12-16 14:18:30 +0100

    #await afp.read

--- aiofile.py
+++ aiofile.py
@@ -1,13 +1,25 @@
-#basic example
+# basic example
 
 import asyncio
 from aiofile import async_open
+
 
 async def main():
     async with async_open("hello.txt", 'w+') as afp:
         await afp.write("Hello ")
         await afp.write("world\n")
         await afp.write("Hello from - async world!")
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+
+# Podejście await afp.read()
+
+
+async def main():
+    async with async_open("hello.txt", 'r') as afp:
+        print(await afp.read())
 
 if __name__ == '__main__':
     asyncio.run(main())
