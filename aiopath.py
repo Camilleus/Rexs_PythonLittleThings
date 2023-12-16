ae2266ecfdf785ae5ad3d4780491db7d3d2c23eb
CamilleusRex <camilleusrex@gmail.com>
2023-12-16 14:25:28 +0100

    added description

---
+++ aiopath.py
@@ -0,0 +1,19 @@
+# Pakiet aiopath
+    """
+    If you're writing asynchronous Python code and want to take advantage of pathlib's capabilities, 
+    but don't want to mix blocking and non-blocking I/O, you can use [aiopath](https://github.com/alexdelorenzo/aiopath). 
+    The aiopath API is exactly the same as the pathlib API, but all necessary methods are asynchronous.
+    """
+
+import asyncio
+from aiopath import AsyncPath
+
+
+async def main():
+    apath = AsyncPath("hello.txt")
+    print(await apath.exists())
+    print(await apath.is_file())
+    print(await apath.is_dir())
+
+if __name__ == '__main__':
+    asyncio.run(main())
