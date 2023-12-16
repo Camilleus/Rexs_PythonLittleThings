92d195e77d2e1cd62c53eea77788d0c894b6765c
CamilleusRex <camilleusrex@gmail.com>
2023-12-16 14:16:28 +0100

    added concurrent.futures

--- PararrelIO-Bound.py
+++ PararrelIO-Bound.py
@@ -1,5 +1,28 @@
 # basic example
 
+# import asyncio
+# import requests
+# from time import time
+
+# urls = ['http://www.google.com',
+#         'http://www.python.org', 'http://duckduckgo.com']
+
+
+# def preview_fetch(url):
+#     r = requests.get(url)
+#     return url, r.text[:150]
+
+
+# if __name__ == '__main__':
+#     start = time()
+#     for url in urls:
+#         r = preview_fetch(url)
+#         print(r)
+#     print(time() - start)
+
+
+# Added concurrent.futures
+
 from concurrent.futures import ThreadPoolExecutor
 import asyncio
 import requests
@@ -14,9 +37,17 @@ def preview_fetch(url):
     return url, r.text[:150]
 
 
+async def preview_fetch_async():
+    loop = asyncio.get_running_loop()
+
+    with ThreadPoolExecutor(3) as pool:
+        futures = [loop.run_in_executor(
+            pool, preview_fetch, url) for url in urls]
+        result = await asyncio.gather(*futures, return_exceptions=True)
+        return result
+
 if __name__ == '__main__':
     start = time()
-    for url in urls:
-        r = preview_fetch(url)
-        print(r)
+    r = asyncio.run(preview_fetch_async())
+    print(r)
     print(time() - start)
