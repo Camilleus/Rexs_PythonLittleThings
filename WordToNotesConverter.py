0d220e17ffff12e1d9d02b5bf2bb827a35e610d7
CamilleusRex <camilleusrex@gmail.com>
2024-02-21 20:04:44 +0100

    added Word to Notes Converter

---
+++ WordToNotesConverter.py
@@ -0,0 +1,2 @@
+import pywhatkit
+pywhatkit.text_to_handwriting("A quickly learning young man, not afraid of hard work hard talk and hard challenges, Interested in foreign languagues, programming (both independently and through the GoIT course), crypto market, stock exchange and project management", save_to="YourText", rgb=(0, 0, 225))
